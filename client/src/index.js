1dc21c8eb01598eaa0d530d4ba581f8bf4bf0028
Alek <56727628+aseawright1@users.noreply.github.com>
2020-06-14 09:08:57 -0500

    fixing og tags

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+// import ReactDOM from 'react-dom';
+import { render } from 'react-snapshot';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
@@ -15,7 +16,8 @@ import './assets/css/style.css';
 import './assets/css/responsive.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
 import 'popper.js';
-ReactDOM.render(<App />, document.getElementById('root'));
+// ReactDOM.render(<App />, document.getElementById('root'));
+render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
